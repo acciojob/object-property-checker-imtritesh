655091f838e7a94b0d219a4e9701383a9d74d001
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-04-15 00:39:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
 function hasKey(key) {
-  //   write your code here
+  if (typeof key !== "string") {
+    throw new Error("Key must be a string");
+  }
+
+  return key in this;
 }
 
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// function hasKey(key) {
+//   //   write your code here
+// }
+
+// // Do not change the code below
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
